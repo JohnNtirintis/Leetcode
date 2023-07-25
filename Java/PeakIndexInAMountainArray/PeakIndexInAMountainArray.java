9853c738859225e1f08c62618bed65e7559c8451
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2023-07-25 15:47:17 +0300

    Daily Problem 25-07-2023 Peak Index in a Mountain Array

---
+++ Java/PeakIndexInAMountainArray/PeakIndexInAMountainArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0, end = arr.length - 1; 
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            // Right
+            if(arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            } 
+            // left
+            else {
+                end = mid;
+            }
+        }
+
+        return start;
+    }
+}
